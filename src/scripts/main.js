a970e9806166978be1b569716fe23427998d4403
Ernie Bello <ernie@ern.me>
2017-07-26 01:38:45 -0400

    mobile styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,16 @@ import { ready, debug } from './utils';
 
 function onReady(e) {
   debug('info', `Event: ${e.type}`, `Datestamp: ${this.date}`);
+
+  // Look for .hamburger
+  const hamburger = document.querySelector('.hamburger');
+  const primarynav = document.querySelector('.primary-nav');
+  // On click
+  hamburger.addEventListener('click', () => {
+    // Toggle class "is-active"
+    hamburger.classList.toggle('is-active');
+    primarynav.classList.toggle('is-active');
+  });
 }
 
 ready(onReady, {
